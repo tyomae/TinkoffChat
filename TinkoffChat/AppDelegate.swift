98dd32e9591f8103d59dde0d7a083f9168031791
Artem <dom1nuk@yandex.ru>
2020-02-15 11:00:33 +0300

    Homework #1

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -12,27 +12,34 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+ 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        print("Not running application moved to Foreground: \(#function)")
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("Application moved from Inactive to Active: \(#function)")
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("Application moved from Active to InActive: \(#function)")
     }
-
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("Application moved from InActive to Background: \(#function)")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("Application moved from Background to Foreground: \(#function)")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("Application suspended: \(#function)")
+    }
+    
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        print(#function)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
+         print(#function)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        print(#function)
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        print(#function)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+         print(#function)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+         print(#function)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+         print(#function)
+    }
 }
 
