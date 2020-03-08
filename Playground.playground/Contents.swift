e5a4c9d15b2a919d060aadaafb05f3048b555410
Artem <dom1nuk@yandex.ru>
2020-03-08 17:17:47 +0300

    Added names

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,3 +1,13 @@
+protocol Person {
+    var name: String { get }
+}
+
+extension Person {
+    var nameWithPosition: String {
+        return "\(Self.self) \(name)"
+    }
+}
+
 class Company {
     let ceo: CEO
     
@@ -6,17 +16,21 @@ class Company {
     }
     
     deinit {
-        print("\(self) deallocating")
+        print("Компания deallocating")
     }
 }
 
-class CEO {
+class CEO: Person {
     
     let productManager: ProductManager
     
+    var name: String {
+        return "Иван Начальников"
+    }
+    
     lazy var printProductManager = { [weak self] in
         guard let productManager = self?.productManager else { return }
-        print("\(productManager)")
+        print("\(productManager.nameWithPosition)")
     }
     lazy var printAllDevelopers = { [weak self] in
         guard let self = self else { return }
@@ -34,13 +48,17 @@ class CEO {
     }
     
     deinit {
-        print("\(self) deallocating")
+        print("\(self.nameWithPosition) deallocating")
     }
 }
 
-class ProductManager {
+class ProductManager: Person {
     
     weak var ceo: CEO?
+    
+    var name: String {
+        return "Петр Менеджеров"
+    }
     
     let developer1: Developer1
     let developer2: Developer2
@@ -54,57 +72,70 @@ class ProductManager {
     }
     
     func printDevelopers() {
-        print("Developers:\n\(developer1)\n\(developer2)")
+        print("Developers:\n\(developer1.nameWithPosition)\n\(developer2.nameWithPosition)")
     }
     
     func printCompany() {
         guard let ceo = ceo else { return }
         
-        print("Company:\n\(ceo)\n\(self)\n\(developer1)\n\(developer2)")
+        print("Company:\n\(ceo.nameWithPosition)\n\(self.nameWithPosition)\n\(developer1.nameWithPosition)\n\(developer2.nameWithPosition)")
     }
     
     deinit {
-        print("\(self) deallocating")
+        print("\(self.nameWithPosition) deallocating")
     }
 }
 
-class Developer {
+class Developer: Person {
     
     weak var productManager: ProductManager?
+    var name: String {
+        return ""
+    }
     
     func askProjectManagerAboutTechnicalSpecification() {
         guard let projectManager = productManager else { return }
-        print("\(self): \(projectManager), жду ТЗ!")
+        print("\(self.name): \(projectManager.name), жду ТЗ!")
     }
     
     func askProjectManagerAboutNewTask() {
         guard let projectManager = productManager else { return }
-        print("\(self): \(projectManager), жду новую задачу!")
+        print("\(self.name): \(projectManager.name), жду новую задачу!")
     }
     
     func askCEOAboutSalary() {
         guard let ceo = productManager?.ceo else { return }
-        print("\(self): \(ceo), вот у другого девелопера 300к в час, хочу больше!")
+        print("\(self.name): \(ceo.name), вот у другого девелопера 300к в час, хочу больше!")
     }
     
     deinit {
-        print("\(self) deallocating")
+        print("\(self.nameWithPosition) deallocating")
     }
 }
 
 class Developer1: Developer {
-    
+
+    override var name: String {
+        return "Олег Программистов"
+    }
+
     func askAnotherDeveloperToPRReview() {
         guard let developer2 = productManager?.developer2 else { return }
-        print("\(self): \(developer2), проверь мой PR, плиз")
+        guard let productManager = productManager else { return }
+        print("\(productManager.developer1.name): \(developer2.name), проверь мой PR, плиз")
     }
 }
 
 class Developer2: Developer {
-    
+
+    override var name: String {
+        return "Вадим Кодов"
+    }
+
     func sayAboutPR() {
         guard let developer1 = productManager?.developer1 else { return }
-        print("\(self): \(developer1), опять наговнокодил :C")
+        guard let productManager = productManager else { return }
+        print("\(productManager.developer2.name): \(developer1.name), опять наговнокодил :C")
     }
 }
 
@@ -139,4 +170,6 @@ func simulateCompany() {
 }
 
 simulateCompany()
+
+
 
